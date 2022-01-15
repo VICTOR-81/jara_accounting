f432e953f6316a73704ed4163206f1ed996e771d
VICTOR-81 <v161181@gmail.com>
2022-01-15 21:16:59 +0700

    continue

---
+++ app/js/formfeedback.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const tabs = document.querySelector('.formfeedback__tabs');
+	const content = document.querySelectorAll('.formfeedback__form');
+
+	const changeClass = (el) => {
+		for (let i = 0; i < tabs.children.length; i++) {
+			tabs.children[i].classList.remove('active');
+		}
+		el.classList.add('active');
+	};
+
+	tabs.addEventListener('click', (e) => {
+		if (e.target.dataset.btn) {
+			const currTab = e.target.dataset.btn;
+			changeClass(e.target);
+			for (let i = 0; i < content.length; i++) {
+				content[i].classList.remove('active');
+				if (content[i].dataset.content === currTab) {
+					content[i].classList.add('active');
+				}
+			}
+		}
+	});
+});
