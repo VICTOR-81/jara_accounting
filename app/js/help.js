5f0357bf336a704e2bfb218e9dc4302695ab87c9
VICTOR-81 <v161181@gmail.com>
2022-01-27 23:41:42 +0700

    continue

--- app/js/help.js
+++ app/js/help.js
@@ -4,13 +4,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
   help.addEventListener('click', function (e) {
 		const target = e.target.closest('div');
-		if (target.classList.contains('help__item-title') && !target.classList.contains('list_item___open')) {
-			target.parentElement.children[1].style.maxHeight =
-				target.parentElement.children[1].children.length * target.parentElement.children[1].children[0].offsetHeight + 'px';
-			target.classList.add('list_item___open');
-		} else if (target.classList.contains('help__item-title') && target.classList.contains('list_item___open')) {
+		if (target.classList.contains('help__item-title') && !target.classList.contains('rotate')) {
+			target.parentElement.children[1].style.maxHeight = 200 + 'px';
+			target.classList.add('rotate');
+		} else if (target.classList.contains('help__item-title') && target.classList.contains('rotate')) {
 			target.parentElement.children[1].style.maxHeight = 0;
-			target.classList.remove('list_item___open');
+			target.classList.remove('rotate');
 		}
 	});
 
